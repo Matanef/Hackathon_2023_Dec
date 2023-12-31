2e5642cf2db9c40f64bcc1a4510e5358893953fe
Matanef <efratimatan@gmail.com>
2023-12-31 21:16:17 +0200

    Hackathon 2023 Dec - first visualisation

--- 31.12/script.js
+++ 31.12/script.js
@@ -1,43 +1,43 @@
+// Function to show the contact form
 function showForm() {
-    const form = document.getElementById('volunteerForm');
-    form.classList.remove('hidden');
-}
-
-function saveToTextFile() {
-    const firstName = document.getElementById('firstName').value;
-    const lastName = document.getElementById('lastName').value;
-    const phoneNumber = document.getElementById('phoneNumber').value;
-    const issue = document.getElementById('issue').value;
-
-    // You can implement a logic here to save the information to a text file or handle it as needed
-
-    // Show popup
-    showPopup();
-}
-
-function handleSearch() {
-    const searchTerm = document.getElementById('search').value.toLowerCase();
-    const slider = document.getElementById('slider');
-    const charityCard = document.getElementById('charityCard');
-
-    // Logic to handle search and display cards accordingly
-    // For simplicity, showing/hiding cards is demonstrated here
-    if (searchTerm === 'specificcharity') {
-        slider.classList.add('hidden');
-        charityCard.classList.remove('hidden');
-    } else {
-        slider.classList.remove('hidden');
-        charityCard.classList.add('hidden');
+    const dropdown = document.getElementById('professionDropdown');
+    const form = document.getElementById('contactForm');
+    
+    if (dropdown.value !== "") {
+        form.classList.remove('hidden');
     }
 }
 
-function submitDonation() {
-    // Logic to handle donation submission
-    // You can implement your own logic or integrate with a payment gateway
-    showPopup();
+// Function to submit the contact form
+function submitForm() {
+    const form = document.getElementById('contactForm');
+    const firstName = document.getElementById('firstName').value;
+    const lastName = document.getElementById('lastName').value;
+    const phone = document.getElementById('phone').value;
+    const issue = document.getElementById('issue').value;
+
+    // Assuming you have a server-side script to handle saving data
+    // Here, you can use JavaScript to save the data to a text file
+    // This is just a basic example and may not work in real-world scenarios
+    const data = `First Name: ${firstName}\nLast Name: ${lastName}\nPhone: ${phone}\nIssue: ${issue}\n\n`;
+    
+    // Save the data to a text file (not recommended for real-world use)
+    // This is just a basic example and may not work in real-world scenarios
+    // You might need a server-side script to handle this properly
+    const blob = new Blob([data], { type: 'text/plain' });
+    const a = document.createElement('a');
+    a.href = URL.createObjectURL(blob);
+    a.download = 'information.txt';
+    a.click();
+
+    // You can also send the data to a server using AJAX or fetch for real-world scenarios
+
+    // Reset the form
+    form.reset();
 }
 
-function showPopup() {
-    const popup = document.getElementById('popup');
-    popup.classList.remove('hidden');
+// Function to handle donation
+function donate() {
+    // Handle donation logic here
+    alert('Donation submitted!');
 }
