27389a1f1ba5b673ed93846b2383cf81c319284a
Matanef <efratimatan@gmail.com>
2024-01-04 01:48:02 +0200

    Hackathon 2023 Dec - Donate 2.0 slide not working still search bar done search not working 04/01/2024 01:47

--- 31.12/donate/donate.js
+++ 31.12/donate/donate.js
@@ -32,3 +32,17 @@ var swiper = new Swiper(".swiper", {
       }
     });
   }
+  function donate() {
+    const form = document.getElementById('contactForm');
+    const userAmount = document.getElementById('userAmount').value;
+    const kind = document.getElementById('kind').value;
+    const data = `Amount user donated: ${userAmount.toString()}\nIssue: ${kind}\n\n`;
+    const blob = new Blob([data], { type: 'text/plain' });
+    const a = document.createElement('a');
+    a.href = URL.createObjectURL(blob);
+    a.download = 'information.txt';
+    a.click();
+
+
+    form.reset();
+}
